2ad9743363184d9c07ee091e827c9d03e16df427
Md Anzar Ahmad <anzarhps@gmail.com>
2023-12-14 22:49:26 +0530

    Initial commit

---
+++ src/components/theme-toggle.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import * as React from "react"
+import { useTheme } from "next-themes"
+
+import { Label } from "@/components/ui/label"
+import { Switch } from "@/components/ui/switch"
+
+export function ThemeToggle() {
+  const { theme, setTheme } = useTheme()
+
+  return (
+    <div className="flex items-center justify-between space-x-2 px-2">
+      <Label htmlFor="theme-mode">Toggle theme</Label>
+      <Switch
+        id="theme-mode"
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      />
+    </div>
+  )
+}
